a0981f0a589c1aafa7a8c212f99d24c4e3306cf1
David Harting <davideharting@gmail.com>
2020-05-18 08:22:34 -0400

    Persist to local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,11 +3,38 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import sessionReducer from "../features/session/sessionSlice";
 
+// Approach for persisting state came from Dan Abramov himself 🙏
+// https://egghead.io/lessons/javascript-redux-persisting-the-state-to-the-local-storage
+const saveState = (state: { session: Object; counter: Object }) => {
+  try {
+    const serializedState = JSON.stringify({
+      session: state.session,
+    });
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    console.error("Unable to persist state to local storage.", err);
+  }
+};
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (err) {
+    return undefined;
+  }
+};
+
 export const store = configureStore({
+  preloadedState: loadState(),
   reducer: {
     counter: counterReducer,
     session: sessionReducer,
   },
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export type RootState = ReturnType<typeof store.getState>;
