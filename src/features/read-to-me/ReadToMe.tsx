ace1f7eaac6cccec79911acd06f1bd7b2b8229f0
David Harting <davideharting@gmail.com>
2020-07-28 22:34:23 -0400

    disable while playing

--- src/features/read-to-me/ReadToMe.tsx
+++ src/features/read-to-me/ReadToMe.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cn from "classnames";
 import { useSelector } from "react-redux";
 import { selectAudioForCurrentBookPage } from "./readToMeSlice";
 import styles from "./read-to-me.module.css";
@@ -6,25 +7,32 @@ import SpeakerIcon from "./speaker-icon.png";
 
 function ReadToMe({ style }: ReadToMeProps) {
   const audioAssetUrls = useSelector(selectAudioForCurrentBookPage);
+  const [disabled, setDisabled] = React.useState<boolean>(false);
 
   if (!audioAssetUrls || audioAssetUrls.length < 1) {
     return null;
   }
 
   const playAudio = () => {
-    // Big limitation - assuming at most 2 audio files!
-    const first = new Audio(audioAssetUrls[0]);
-    first.onended = () => {
-      const second = new Audio(audioAssetUrls[1]);
-      second.play();
-    };
-    first.play();
+    if (!disabled) {
+      // Big limitation - assuming at most 2 audio files!
+      setDisabled(true);
+      const first = new Audio(audioAssetUrls[0]);
+      first.onended = () => {
+        const second = new Audio(audioAssetUrls[1]);
+        second.onended = () => {
+          setDisabled(false);
+        };
+        second.play();
+      };
+      first.play();
+    }
   };
 
   return (
     <img
       alt="Play audio"
-      className={styles.speaker}
+      className={disabled ? styles.disabledSpeaker : styles.speaker}
       style={style}
       src={SpeakerIcon}
       role="button"
