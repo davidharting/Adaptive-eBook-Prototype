653b18dc04495fe75fe1ea4637d106ede5ab7dd2
David Harting <davideharting@gmail.com>
2020-08-11 17:38:21 -0400

    center narrative text

--- src/features/read/components/Prompt.tsx
+++ src/features/read/components/Prompt.tsx
@@ -3,7 +3,7 @@ import { IPrompt } from "types/generated/contentful";
 
 function Prompt({ prompt }: PromptProps) {
   return (
-    <p className="mb-0" style={{ fontSize: "2.5rem" }}>
+    <p className="mb-0 text-center" style={{ fontSize: "2.5rem" }}>
       {prompt.fields.text}
     </p>
   );
