c17ef79f06da3605a4e7597480e44b885752e520
David Harting <davideharting@gmail.com>
2020-08-11 20:30:13 -0400

    rename Page to BookPage

--- src/features/read/components/BookPage.tsx
+++ src/features/read/components/BookPage.tsx
@@ -4,15 +4,15 @@ import { IBookPage } from "models/BookPage";
 import Prompt from "./Prompt";
 import Question from "./Question";
 
-interface PageProps {
-  page: IBookPage;
+interface BookPageProps {
+  bookPage: IBookPage;
 }
 
 function isPage(page: IBookPage): page is IPage {
   return page.sys.contentType.sys.id === "page";
 }
 
-function Page({ page }: PageProps) {
+function Page({ bookPage: page }: BookPageProps) {
   if (isPage(page)) {
     return <Prompt prompt={page.fields.narrative} />;
   }
