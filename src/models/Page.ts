ed97d773d2b6d004f8326170bbb5ed5a300dc315
David Harting <david@Davids-MacBook-Pro.local>
2020-09-01 19:45:00 -0400

    Add fields for prompts per treatment type

--- src/models/Page.ts
+++ src/models/Page.ts
@@ -1,9 +1,24 @@
-import { IPage } from "types/generated/contentful";
+import { IPage, IPrompt } from "types/generated/contentful";
+import { Treatment } from "features/setup-device/setupDeviceSlice";
 
 class Page {
   static getAudio(page: IPage): string[] {
     const promptAudio = page.fields.narrative.fields.audio?.fields.file.url;
     return promptAudio ? [promptAudio] : [];
+  }
+
+  static getPrompt(page: IPage, treatment: Treatment): IPrompt | null {
+    switch (treatment) {
+      case "mixed": {
+        return page.fields.narrative;
+      }
+      case "number": {
+        return page.fields.numberNarrative || null;
+      }
+      case "size": {
+        return page.fields.sizeNarrative || null;
+      }
+    }
   }
 }
 
