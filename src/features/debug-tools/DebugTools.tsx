2aa8285ecaa26fdb2fa910d883103bce4c9d4022
David Harting <david@Davids-MBP.local>
2020-09-10 18:11:19 -0400

    Add "reset" button to dev tools

--- src/features/debug-tools/DebugTools.tsx
+++ src/features/debug-tools/DebugTools.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useBooleanQueryParam } from "lib/browser/query-params";
+import { signOut } from "features/setup-device/setupDeviceSlice";
+
 import styles from "./debug-tools.module.css";
 import { selectDebugData } from "./debugToolsSlice";
 
 function DebugTools() {
+  const dispatch = useDispatch();
   const shouldShowDebugTools = useBooleanQueryParam("debug");
   const data = useSelector(selectDebugData);
 
@@ -13,16 +16,21 @@ function DebugTools() {
   }
 
   return (
-    <div className={styles.container}>
-      {data.map((d, i) => {
-        return (
-          <div key={i} className={styles.row}>
-            <span>{d.key}:&nbsp;</span>
-            <span>{d.value}</span>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className={styles.container}>
+        {data.map((d, i) => {
+          return (
+            <div key={i} className={styles.row}>
+              <span>{d.key}:&nbsp;</span>
+              <span>{d.value}</span>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.reset}>
+        <button onClick={() => dispatch(signOut())}>Reset</button>
+      </div>
+    </>
   );
 }
 
